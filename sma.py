e08054c2e8ce1c89d3bfc5659b6e8b849f5ba8e2
PatrickHennis <patrickhennis@gmail.com>
2017-09-11 23:00:31 -0400

    add nonworking bot

---
+++ sma.py
@@ -0,0 +1,36 @@
+from bittrex_v2 import Bittrex
+b = Bittrex()
+
+trade = 'BTC'
+currency = 'ZEN'
+market = '{0}-{1}'.format(trade, currency)
+
+zen_summary = b.get_market_summary(market)
+zen_price_last = zen_summary['result']['Last']
+
+ticks = b.get_ticks(market, "day")
+
+length_of_ticks = len(ticks['result'])
+
+num_of_days = 20
+curr_day = 1
+
+running_ave = 0
+num = 0
+# calc sma
+while(curr_day < num_of_days):
+    current = length_of_ticks - curr_day - 1
+    running_ave += ticks['result'][current]['C']
+    curr_day += 1
+    num += 1
+
+running_ave += zen_price_last
+
+print(running_ave/(num_of_days))
+print(num)
+
+# calc standard deviation
+curr_day = 0
+squared_deviations = 0
+
+# while(curr_day < num_of_days):
